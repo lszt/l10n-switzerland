bd16ed1ed848c99ee283ae8a406ad40b309006eb
Philipp Hug <philipp@hug.cx>
2020-09-11 22:10:28 +0200

    drop dependency to account_payment_line_cancel

--- l10n_ch_import_pain002/__manifest__.py
+++ l10n_ch_import_pain002/__manifest__.py
@@ -10,7 +10,6 @@
     'depends': [
         'account_payment_order',
         'account_bank_statement_import_camt',
-        'account_payment_line_cancel',
     ],
     'demo': [
         'test_files/test_data.yml',
